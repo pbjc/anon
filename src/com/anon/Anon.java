789b038ccb0c224b0590826764c79b7dd9eb43bd
samm81 <samwmaynard@gmail.com>
2014-10-19 02:08:54 -0500

    refactoring a bit

--- src/com/anon/Anon.java
+++ src/com/anon/Anon.java
@@ -2,8 +2,12 @@ package com.anon;
 
 import android.app.Application;
 
+import com.anon.backend.Comment;
+import com.anon.backend.Group;
+import com.anon.backend.Post;
 import com.parse.Parse;
 import com.parse.ParseACL;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
 
 public class Anon extends Application {
@@ -12,6 +16,10 @@ public class Anon extends Application {
     public void onCreate() {
         super.onCreate();
 
+        ParseObject.registerSubclass(Group.class);
+        ParseObject.registerSubclass(Post.class);
+        ParseObject.registerSubclass(Comment.class);
+        
         // Add your initialization code here
         Parse.initialize(this, "03BKZLIMEoWsiabvkIpaDWHYCRApQwobb4tOKeBo",
                 "A49RaubTM6QJETtEJllPvnCqE4mcWJhTXh4GvplJ");
