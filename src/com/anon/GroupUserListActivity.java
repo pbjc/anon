c8c7962a56083ded0679749584662b7adb759c1f
Paul Choi <choipaul96@gmail.com>
2014-10-19 09:43:44 -0500

    Only the admin can add members, admin now distinguished in member list

--- src/com/anon/GroupUserListActivity.java
+++ src/com/anon/GroupUserListActivity.java
@@ -16,7 +16,7 @@ import com.parse.ParseUser;
 public class GroupUserListActivity extends Activity {
 
 	ListView userList;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -24,33 +24,36 @@ public class GroupUserListActivity extends Activity {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE); // removes title bar
 		setContentView(R.layout.group_users_list_activity);
 		initializeVars();
-		
+
 		Bundle b = getIntent().getExtras();
 		String groupID = b.getString("groupID");
-		
+		String admin = b.getString("admin");
+
 		Group group = null;
 		List<ParseUser> groupMemberList = null;
 		List<String> userArrayList = new ArrayList<String>();
-		
+
 		try {
 			group = Group.getGroupFromID(groupID);
 			groupMemberList = group.getMembers();
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		for (ParseUser user : groupMemberList) {
-			userArrayList.add(user.getString("name"));
+			if (user.getUsername().equals(admin))
+				userArrayList.add(user.getString("name") + " (admin)");
+			else
+				userArrayList.add(user.getString("name"));
 		}
-		
-		
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, userArrayList);
-		
+
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, userArrayList);
+
 		userList.setAdapter(arrayAdapter);
-		
+
 	}
-	
+
 	private void initializeVars() {
 		userList = (ListView) findViewById(R.id.lvGroupUserList);
 	}
--- src/com/anon/PostsActivity.java
+++ src/com/anon/PostsActivity.java
@@ -27,7 +27,9 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
-public class PostsActivity extends Activity implements EditNameDialogListenerNewPosts, EditNameDialogListenerAddUsers {
+public class PostsActivity extends Activity implements
+		EditNameDialogListenerNewPosts, EditNameDialogListenerAddUsers {
+	private ParseUser admin = null;
 
 	@Override
 	public void onCreate(Bundle b) {
@@ -46,85 +48,98 @@ public class PostsActivity extends Activity implements EditNameDialogListenerNew
 			e.printStackTrace();
 		}
 
-        
 		int count = 0;
-        for(Post post : posts){
-            RelativeLayout layout = new RelativeLayout(this);
-            
-            RelativeLayout.LayoutParams  textParams =
-                    new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT),
-                    numParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-            
-            TextView text = new TextView(this), numPosts = new TextView(this);
-            text.setText(post.getMessage());
-            text.setTypeface(Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf"));
-            text.setTextSize(30);
-            text.setTextColor(0xff000000);
-            text.setId(10 + count);
-            text.setLayoutParams(textParams);
-            
-            try {
-                numPosts.setText(post.getAllComments().size()+" comments");
-            } catch (ParseException e) {}
-            numPosts.setTypeface(Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf"));
-            numPosts.setTextSize(15);
-            numPosts.setTextColor(0xff7f7f7f);
-            numParams.addRule(RelativeLayout.BELOW, 10 + count);
-            numPosts.setLayoutParams(numParams);
-            
-            layout.addView(text);
-            layout.addView(numPosts);
-            layout.setPadding(40, 40, 40, 40);
-            
-            ret.put(layout, post.getObjectId());
-            count++;
-        }
-        
-        return ret;
-    }
-    
-    private void setupGUI(String parentGroupID){
-        try {
-            TextView title = ((TextView)findViewById(R.id.tvGroup));
-            title.setText(Group.getGroupFromID(parentGroupID).getName());
-            title.setTypeface(Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf"));
-        } catch (ParseException e) {
-            Log.wtf("Shit", e+"");
-        }
-        
-        final LinkedHashMap<View, String> posts = loadPosts(parentGroupID);
-        
-        for(final View line : posts.keySet()){
-            LinearLayout layout = ((LinearLayout)findViewById(R.id.llPosts));
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-            params.setMargins(0, 20, 0, 20);
+		for (Post post : posts) {
+			RelativeLayout layout = new RelativeLayout(this);
 
+			RelativeLayout.LayoutParams textParams = new RelativeLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT), numParams = new RelativeLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 
-            line.setLayoutParams(params);
-            line.setClickable(true);
-            line.setOnClickListener(new OnClickListener() {
-                public void onClick(View v) {
-                    Intent intent = new Intent(PostsActivity.this,
-                            CommentsActivity.class);
-                    Bundle groupInfo = new Bundle();
-                    groupInfo.putString("parentPostID", posts.get(line));
-                    intent.putExtras(groupInfo);
-                    startActivity(intent);
-                }
-            });
-            line.setBackground(getResources().getDrawable(R.drawable.post_background));
-            
-            layout.addView(line);
-        }
-    }
+			TextView text = new TextView(this), numPosts = new TextView(this);
+			text.setText(post.getMessage());
+			text.setTypeface(Typeface.createFromAsset(getAssets(),
+					"Roboto-Light.ttf"));
+			text.setTextSize(30);
+			text.setTextColor(0xff000000);
+			text.setId(10 + count);
+			text.setLayoutParams(textParams);
+
+			try {
+				numPosts.setText(post.getAllComments().size() + " comments");
+			} catch (ParseException e) {
+			}
+			numPosts.setTypeface(Typeface.createFromAsset(getAssets(),
+					"Roboto-Light.ttf"));
+			numPosts.setTextSize(15);
+			numPosts.setTextColor(0xff7f7f7f);
+			numParams.addRule(RelativeLayout.BELOW, 10 + count);
+			numPosts.setLayoutParams(numParams);
+
+			layout.addView(text);
+			layout.addView(numPosts);
+			layout.setPadding(40, 40, 40, 40);
+
+			ret.put(layout, post.getObjectId());
+			count++;
+		}
+
+		return ret;
+	}
+
+	private void setupGUI(String parentGroupID) {
+		try {
+			TextView title = ((TextView) findViewById(R.id.tvGroup));
+			title.setText(Group.getGroupFromID(parentGroupID).getName());
+			title.setTypeface(Typeface.createFromAsset(getAssets(),
+					"Roboto-Light.ttf"));
+		} catch (ParseException e) {
+			Log.wtf("Shit", e + "");
+		}
+
+		final LinkedHashMap<View, String> posts = loadPosts(parentGroupID);
+
+		for (final View line : posts.keySet()) {
+			LinearLayout layout = ((LinearLayout) findViewById(R.id.llPosts));
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+					LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+			params.setMargins(0, 20, 0, 20);
+
+			line.setLayoutParams(params);
+			line.setClickable(true);
+			line.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					Intent intent = new Intent(PostsActivity.this,
+							CommentsActivity.class);
+					Bundle groupInfo = new Bundle();
+					groupInfo.putString("parentPostID", posts.get(line));
+					intent.putExtras(groupInfo);
+					startActivity(intent);
+				}
+			});
+			line.setBackground(getResources().getDrawable(
+					R.drawable.post_background));
+
+			layout.addView(line);
+
+			try {
+				admin = Group.getGroupFromID(parentGroupID).getAdmin().fetch();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.posts_page_activity_menu, menu);
+		if (admin.getUsername()
+				.equals(ParseUser.getCurrentUser().getUsername()))
+			menu.removeItem(R.id.mbPostsAddNewUser);
 		return super.onCreateOptionsMenu(menu);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -139,16 +154,20 @@ public class PostsActivity extends Activity implements EditNameDialogListenerNew
 			addNewUserDialog.show(fm2, "addNewUserDialog");
 			return true;
 		case R.id.mbPostsListAllUsers:
-			Intent i = new Intent(PostsActivity.this, GroupUserListActivity.class);
+			Intent i = new Intent(PostsActivity.this,
+					GroupUserListActivity.class);
 			Bundle b = new Bundle();
-			
+
 			Group group = null;
 			try {
-				group = Group.getGroupFromID(getIntent().getExtras().getString("parentGroupID"));
+				group = Group.getGroupFromID(getIntent().getExtras().getString(
+						"parentGroupID"));
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
-			b.putString("groupID", getIntent().getExtras().getString("parentGroupID"));
+			b.putString("groupID",
+					getIntent().getExtras().getString("parentGroupID"));
+			b.putString("admin", admin.getUsername());
 			i.putExtras(b);
 			startActivity(i);
 			return true;
@@ -162,32 +181,34 @@ public class PostsActivity extends Activity implements EditNameDialogListenerNew
 		String postText = inputText;
 		Group group = null;
 		try {
-			group = Group.getGroupFromID(getIntent().getExtras().getString("parentGroupID"));
-		} catch(ParseException e) {
+			group = Group.getGroupFromID(getIntent().getExtras().getString(
+					"parentGroupID"));
+		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		new Post(postText, group, ParseUser.getCurrentUser());
 		finish();
-        startActivity(getIntent());
+		startActivity(getIntent());
 	}
 
 	@Override
 	public void onFinishEditDialogAddUsers(String inputText) {
 		Group group = null;
 		try {
-			group = Group.getGroupFromID(getIntent().getExtras().getString("parentGroupID"));
-		} catch(ParseException e) {
+			group = Group.getGroupFromID(getIntent().getExtras().getString(
+					"parentGroupID"));
+		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		String addUserEmail = inputText;
 		Log.wtf("Input Text", inputText);
 		ParseQuery<ParseUser> userQuery = ParseUser.getQuery();
 		userQuery.whereEqualTo("email", addUserEmail);
-		
+
 		ParseUser user = null;
 		try {
 			user = userQuery.find().get(0);
-		} catch(ParseException e) {
+		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		group.addMember(user);
